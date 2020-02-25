f32dab0ba5df4309649ecf7d27a9fd165d799988
brunoscapri <bruno.capriglioni@pucpr.edu.br>
2020-02-25 01:08:26 -0300

    sign up screen

--- lib/pages/singup.page.dart
+++ lib/pages/singup.page.dart
@@ -21,19 +21,56 @@ class SignupPage extends StatelessWidget {
             height: 450,
             width: double.infinity,
             padding: EdgeInsets.all(15),
-            decoration: BoxDecoration(color: Colors.white, boxShadow: [
-              new BoxShadow(
-                  color: Colors.black12,
-                  offset: Offset(1, 2.0),
-                  blurRadius: 5,
-                  spreadRadius: 1),
-            ]),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              boxShadow: [
+                new BoxShadow(
+                    color: Colors.black12,
+                    offset: Offset(1, 2.0),
+                    blurRadius: 5,
+                    spreadRadius: 1),
+              ],
+            ),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
                 Text(
                   "Sign up",
-                  style: TextStyle(fontSize: 24),
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                TextFormField(
+                  autofocus: true,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(labelText: "Email"),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  autofocus: true,
+                  obscureText: true,
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(labelText: "Password"),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                Container(
+                  width: double.infinity,
+                  child: FlatButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Sign up",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      color: Colors.blue),
                 )
               ],
             ),
